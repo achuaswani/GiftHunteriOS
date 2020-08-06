f05d64a0909bb78bf82f45a36d2c9461c22ba127
Aswani <achuaswani.g@gmail.com>
2020-08-06 13:05:51 -0700

    Initial Commit

---
+++ GiftHunteriOS/GiftHunteriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftHunteriOSApp.swift
+//  GiftHunteriOS
+//
+//  Created by Aswani G on 8/6/20.
+//
+
+import SwiftUI
+
+@main
+struct GiftHunteriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
